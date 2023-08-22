d379855ca029ccf001e9b6ce8ff4a15305d0e0a7
HusseinHany <hhany3997@gmail.com>
2023-08-22 10:37:04 +0300

    Change book data, add seeds

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const myLibrary = [];
+
+// some seeds
+function seed() {
+    const book1 = new Book('The Hobbit', 'J.R.R. Tolkien', 1937, 310, true);
+    const book2 = new Book('The Fellowship of the Ring', 'J.R.R. Tolkien', 1954, 423, true);
+    const book3 = new Book('The Two Towers', 'J.R.R. Tolkien', 1954, 352, true);
+    const book4 = new Book('The Return of the King', 'J.R.R. Tolkien', 1955, 416, true);
+    const book5 = new Book('The Silmarillion', 'J.R.R. Tolkien', 1977, 365, true);
+    myLibrary.push(book1, book2, book3, book4, book5);
+    myLibrary.forEach(book => book.addToTable());
+}
 const table = document.querySelector('#booksTable tbody');
 // The book obj constructor
-function Book(title, author, year) {
+function Book(title, author, year, numPages, read) {
     this.title = title;
     this.author = author;
     this.year = year;
+    this.numPages = numPages;
+    this.read = read;
 
     this.addToTable = function() {
         // create a table row for each book
@@ -13,14 +26,21 @@ function Book(title, author, year) {
         const title = document.createElement('td');
         const author = document.createElement('td');
         const year = document.createElement('td');
+        const numPages = document.createElement('td');
+        const read = document.createElement('td');
         // add book properties to table data
         title.textContent = this.title;
         author.textContent = this.author;
         year.textContent = this.year;
+        numPages.textContent = this.numPages;
+        // read is a checkbox
+        read.innerHTML = `<input type="checkbox" name="read" id="read" ${this.read ? 'checked' : ''}>`;
         // add table data to table row
         row.appendChild(title);
         row.appendChild(author);
         row.appendChild(year);
+        row.appendChild(numPages);
+        row.appendChild(read);
         // add table row to table body
         table.appendChild(row);
     }
@@ -31,7 +51,9 @@ function addBookToLibrary() {
     const title = prompt('title');
     const author = prompt('author');
     const year = prompt('year');
-    const book = new Book(title, author, year);
+    const numPages = prompt('numPages');
+    const read = prompt('read');
+    const book = new Book(title, author, year, numPages, read);
     myLibrary.push(book);
     // clear table body
     const table = document.querySelector('#booksTable tbody');
@@ -52,3 +74,5 @@ const addBookBtn = document.querySelector('#addBookBtn');
 addBookBtn.addEventListener('click', () => {
     addBookToLibrary();
 });
+
+seed();
