9de3ce7ad165101de387f27a7bceba0cab46b170
HusseinHany <hhany3997@gmail.com>
2023-08-22 10:25:11 +0300

    More optimization

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
-const myLibrary = [{title: 'The Hobbit', author: 'J.R.R. Tolkien', year: '1937'}, {title: 'The Fellowship of the Ring', author: 'J.R.R. Tolkien', year: '1954'}];
-
+const myLibrary = [];
+const table = document.querySelector('#booksTable tbody');
 // The book obj constructor
 function Book(title, author, year) {
     this.title = title;
     this.author = author;
     this.year = year;
+
+    this.addToTable = function() {
+        // create a table row for each book
+        const row = document.createElement('tr');
+        // create a table data for each book property
+        const title = document.createElement('td');
+        const author = document.createElement('td');
+        const year = document.createElement('td');
+        // add book properties to table data
+        title.textContent = this.title;
+        author.textContent = this.author;
+        year.textContent = this.year;
+        // add table data to table row
+        row.appendChild(title);
+        row.appendChild(author);
+        row.appendChild(year);
+        // add table row to table body
+        table.appendChild(row);
+    }
 }
 
 // Get input from user and add to myLibrary array
@@ -24,30 +43,10 @@ function addBookToLibrary() {
 // Display books in myLibrary array
 function displayBooks() {
     // get table body
-    const table = document.querySelector('#booksTable tbody');
     myLibrary.forEach(book => {
-        // create a table row for each book
-        const row = document.createElement('tr');
-        // create a table data for each book property
-        const title = document.createElement('td');
-        const author = document.createElement('td');
-        const year = document.createElement('td');
-        // add book properties to table data
-        title.textContent = book.title;
-        author.textContent = book.author;
-        year.textContent = book.year;
-        // add table data to table row
-        row.appendChild(title);
-        row.appendChild(author);
-        row.appendChild(year);
-        // add table row to table body
-        table.appendChild(row);
+        book.addToTable();
     });
 }
-
-displayBooks();
-
-
 // the logic to add a book to the library
 const addBookBtn = document.querySelector('#addBookBtn');
 addBookBtn.addEventListener('click', () => {
