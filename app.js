a7a362ef91042644a993cf35fffd098097cd901b
HusseinHany <hhany3997@gmail.com>
2023-08-31 17:22:55 +0300

    get controller to work

--- app.js
+++ app.js
@@ -1,10 +1,83 @@
-// const myLibrary = [];
-const dialog = document.querySelector('#addBookDialog');
-const form = document.querySelector('#bookForm');
-const table = document.querySelector('#booksTable tbody');
+class Library {
+    constructor() {
+        this.books = [];
+    }
+    addBook(book) {
+        this.books.push(book);
+        book.addToTable();
+    }
+    removeBook(book) {
+        let index = this.books.indexOf(book);
+        this.books.splice(index, 1);
+        // remove from table
+        let row = myLibrary.table.childNodes[index + 1];
+        myLibrary.table.removeChild(row);
+    }
+    displayBooks() {
+        this.books.forEach(book => {
+            book.addToTable();
+        });
+    }
+}
+class LibraryController {
+    constructor() {
+        this.myLibrary = new Library();
+        this.form = document.querySelector('#bookForm');
+        this.dialog = document.querySelector('#addBookDialog');
+        this.table = document.querySelector('#booksTable tbody');
+        this.addBookBtn = document.querySelector('#addBookBtn');
 
-// Book class
-// tested
+        this.form.addEventListener('submit', (e) => {
+            e.preventDefault();
+            this.addBook();
+        });
+
+        this.addBookBtn.addEventListener('click', this.addBookToLibrary.bind(this));
+
+    }
+    addBookToLibrary() {
+        this.dialog.showModal();
+    }
+    addBook() {
+        const title = document.querySelector('#title').value;
+        const author = document.querySelector('#author').value;
+        const year = document.querySelector('#year').value;
+        const numPages = document.querySelector('#pages').value;
+        const read = document.querySelector('#isRead').checked;
+        let book = new Book(title, author, year, numPages, read);
+        this.myLibrary.addBook(book);
+        this.dialog.close();
+    }
+    removeBook(book) {
+        let index = this.myLibrary.books.indexOf(book);
+        this.myLibrary.books.splice(index, 1);
+        // remove from table
+        let row = this.table.childNodes[index + 1];
+        this.table.removeChild(row);
+    }
+    displayBooks() {
+        this.myLibrary.books.forEach(book => {
+            book.addToTable();
+        });
+
+    }
+
+    seed() {
+        const book1 = new Book('The Hobbit', 'J.R.R. Tolkien', 1937, 310, true);
+        const book2 = new Book('The Fellowship of the Ring', 'J.R.R. Tolkien', 1954, 423, true);
+        const book3 = new Book('The Two Towers', 'J.R.R. Tolkien', 1954, 352, true);
+        const book4 = new Book('The Return of the King', 'J.R.R. Tolkien', 1955, 416, true);
+        const book5 = new Book('The Silmarillion', 'J.R.R. Tolkien', 1977, 365, true);
+        this.myLibrary.addBook(book1);
+        this.myLibrary.addBook(book2);
+        this.myLibrary.addBook(book3);
+        this.myLibrary.addBook(book4);
+        this.myLibrary.addBook(book5);
+    }
+}
+
+let myLibrary = new LibraryController();
+
 class Book {
     constructor(title, author, year, numPages, read) {
         this.title = title;
@@ -37,67 +110,7 @@ class Book {
         row.appendChild(numPages);
         row.appendChild(read);
         // add table row to table body
-        table.appendChild(row);
+        myLibrary.table.appendChild(row);
     }
 }
-
-// library class
-class Library {
-    constructor() {
-        this.books = [];
-    }
-    addBook(book) {
-        this.books.push(book);
-        book.addToTable();
-    }
-    removeBook(book) {
-        let index = this.books.indexOf(book);
-        this.books.splice(index, 1);
-        // remove from table
-        let row = table.childNodes[index + 1];
-        table.removeChild(row);
-    }
-    displayBooks() {
-        this.books.forEach(book => {
-            book.addToTable();
-        });
-    }
-}
-let myLibrary = new Library();
-// some seeds
-function seed() {
-    const book1 = new Book('The Hobbit', 'J.R.R. Tolkien', 1937, 310, true);
-    const book2 = new Book('The Fellowship of the Ring', 'J.R.R. Tolkien', 1954, 423, true);
-    const book3 = new Book('The Two Towers', 'J.R.R. Tolkien', 1954, 352, true);
-    const book4 = new Book('The Return of the King', 'J.R.R. Tolkien', 1955, 416, true);
-    const book5 = new Book('The Silmarillion', 'J.R.R. Tolkien', 1977, 365, true);
-    myLibrary.addBook(book1);
-    myLibrary.addBook(book2);
-    myLibrary.addBook(book3);
-    myLibrary.addBook(book4);
-    myLibrary.addBook(book5);
-}
-
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-    const title = document.querySelector('#title').value;
-    const author = document.querySelector('#author').value;
-    const year = document.querySelector('#year').value;
-    const numPages = document.querySelector('#pages').value;
-    const read = document.querySelector('#isRead').checked;
-    let book = new Book(title, author, year, numPages, read);
-    myLibrary.addBook(book);
-    dialog.close();
-})
-
-// Get input from user and add to myLibrary array
-function addBookToLibrary() {
-    dialog.showModal();
-}
-
-const addBookBtn = document.querySelector('#addBookBtn');
-addBookBtn.addEventListener('click', () => {
-    addBookToLibrary();
-});
-
-seed();
+myLibrary.seed();
