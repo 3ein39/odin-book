03a3b2d5328edd95908f491ecce827566f30b51e
HusseinHany <hhany3997@gmail.com>
2023-08-31 16:42:34 +0300

    Implement Book class

--- app.js
+++ app.js
@@ -1,31 +1,22 @@
 const myLibrary = [];
 const dialog = document.querySelector('#addBookDialog');
 const form = document.querySelector('#bookForm');
-// some seeds
-function seed() {
-    const book1 = new Book('The Hobbit', 'J.R.R. Tolkien', 1937, 310, true);
-    const book2 = new Book('The Fellowship of the Ring', 'J.R.R. Tolkien', 1954, 423, true);
-    const book3 = new Book('The Two Towers', 'J.R.R. Tolkien', 1954, 352, true);
-    const book4 = new Book('The Return of the King', 'J.R.R. Tolkien', 1955, 416, true);
-    const book5 = new Book('The Silmarillion', 'J.R.R. Tolkien', 1977, 365, true);
-    myLibrary.push(book1, book2, book3, book4, book5);
-    myLibrary.forEach(book => book.addToTable());
-}
-const table = document.querySelector('#booksTable tbody');
-// The book obj constructor
-function Book(title, author, year, numPages, read) {
-    this.title = title;
-    this.author = author;
-    this.year = year;
-    this.numPages = numPages;
-    this.read = read;
 
-    this.addToTable = function() {
-        // create a table row for each book
+// Book class
+class Book {
+    constructor(title, author, year, numPages, read) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+        this.numPages = numPages;
+        this.read = read;
+    }
+    addToTable() {
+         // create a table row for each book
         const row = document.createElement('tr');
         row.addEventListener('dblclick', (el) => {
             let title = el.target.innerText;
-            let book = myLibrary.find(book => book.title === title);
+            // let book = myLibrary.find(book => book.title === title);
             // get its index from child nodes
             let index = Array.prototype.indexOf.call(table.childNodes, el.target.parentNode);
             // remove it from the table
@@ -55,6 +46,60 @@ function Book(title, author, year, numPages, read) {
         table.appendChild(row);
     }
 }
+// some seeds
+function seed() {
+    const book1 = new Book('The Hobbit', 'J.R.R. Tolkien', 1937, 310, true);
+    const book2 = new Book('The Fellowship of the Ring', 'J.R.R. Tolkien', 1954, 423, true);
+    const book3 = new Book('The Two Towers', 'J.R.R. Tolkien', 1954, 352, true);
+    const book4 = new Book('The Return of the King', 'J.R.R. Tolkien', 1955, 416, true);
+    const book5 = new Book('The Silmarillion', 'J.R.R. Tolkien', 1977, 365, true);
+    myLibrary.push(book1, book2, book3, book4, book5);
+    myLibrary.forEach(book => book.addToTable());
+}
+const table = document.querySelector('#booksTable tbody');
+// The book obj constructor
+// function Book(title, author, year, numPages, read) {
+//     this.title = title;
+//     this.author = author;
+//     this.year = year;
+//     this.numPages = numPages;
+//     this.read = read;
+//
+//     this.addToTable = function() {
+//         // create a table row for each book
+//         const row = document.createElement('tr');
+//         row.addEventListener('dblclick', (el) => {
+//             let title = el.target.innerText;
+//             let book = myLibrary.find(book => book.title === title);
+//             // get its index from child nodes
+//             let index = Array.prototype.indexOf.call(table.childNodes, el.target.parentNode);
+//             // remove it from the table
+//             table.removeChild(el.target.parentNode);
+//             myLibrary.splice(index, 1);
+//         });
+//         // create a table data for each book property
+//         const title = document.createElement('td');
+//         const author = document.createElement('td');
+//         const year = document.createElement('td');
+//         const numPages = document.createElement('td');
+//         const read = document.createElement('td');
+//         // add book properties to table data
+//         title.textContent = this.title;
+//         author.textContent = this.author;
+//         year.textContent = this.year;
+//         numPages.textContent = this.numPages;
+//         // read is a checkbox
+//         read.innerHTML = `<input type="checkbox" name="read" id="read" ${this.read ? 'checked' : ''}>`;
+//         // add table data to table row
+//         row.appendChild(title);
+//         row.appendChild(author);
+//         row.appendChild(year);
+//         row.appendChild(numPages);
+//         row.appendChild(read);
+//         // add table row to table body
+//         table.appendChild(row);
+//     }
+// }
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
